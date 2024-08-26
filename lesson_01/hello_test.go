15c66f4d4a8ea6de305fb0cfe045f56c5cb93316
Ernestas Šalna <ernestas.salna@teliacompany.com>
2024-08-26 20:17:43 +0300

    Lesson_01: Adding switch statement and a greetingPrefix method

--- lesson_01/hello_test.go
+++ lesson_01/hello_test.go
@@ -4,21 +4,36 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Ernestas")
+		got := Hello("Ernestas", "")
 		want := "Hello, Ernestas"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Kristina", "French")
+		want := "Bonjour, Kristina"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Lithuanian", func(t *testing.T) {
+		got := Hello("Jonas", "Lithuanian")
+		want := "Labas, Jonas"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
